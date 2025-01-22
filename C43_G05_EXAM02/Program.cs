df58d46980f136ae29d4b205a82fd23d0ef481b4
Greg Low <greg@gmail.com>
2025-01-22 20:27:17 +0200

    Add project files.

---
+++ C43_G05_EXAM02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C43_G05_EXAM02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
